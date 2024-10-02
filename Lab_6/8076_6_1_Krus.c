798965b34beb10bbcc21a0f545f7f9ac9df86287
Scorpion <btejanshu@gmail.com>
2024-10-02 18:00:01 +0530

    Edit the code

--- Lab_6/8076_6_1_Krus.c
+++ Lab_6/8076_6_1_Krus.c
@@ -1,66 +1,73 @@
-// Output: 
-//     Edge    Cost 
-    // 8--7     1 
-    // 7--6     2 
-    // 3--9     2 
-    // 1--2     4 
-    // 3--6     4 `
-    // 3--4     7 
-    // 1--8     8 
-    // 4--5     9 
-// Total Weight of the Spanning Tree: 37
-#include<stdio.h>
-#include<stdlib.h>
-struct Edge
-{
-    int u;
-    int v;
-    int weight;
-};
-//Sort the edges in increasing order of their weights
-void sort(struct Edge arr[],int n)
-{
-    struct Edge temp;
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=0;j<n-i-1;j++)
-        {
-            if(arr[j].weight>arr[j+1].weight)
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct {
+    int u, v, w;
+} Edge;
+
+Edge edges[MAX];
+int parent[MAX];
+int n, m;
+
+int find(int i) {
+    while (parent[i] != i)
+        i = parent[i];
+    return i;
+}
+
+void union1(int i, int j) {
+    int a = find(i);
+    int b = find(j);
+    parent[a] = b;
+}
+
+int cmp(const void *a, const void *b) {
+    Edge *edgeA = (Edge *)a;
+    Edge *edgeB = (Edge *)b;
+    return edgeA->w - edgeB->w;
+}
+
+void kruskal() {
+    int i, j, totalCost = 0;
+    Edge result[MAX];
+
+    for (i = 0; i < n; i++)
+        parent[i] = i;
+
+    qsort(edges, m, sizeof(Edge), cmp);
+
+    for (i = 0, j = 0; i < m && j < n - 1; i++) {
+        int u = edges[i].u;
+        int v = edges[i].v;
+        int w = edges[i].w;
+
+        if (find(u) != find(v)) {
+            result[j++] = edges[i];
+            union1(u, v);
+            totalCost += w;
         }
     }
+
+    printf("Edge\tCost\n");
+    for (i = 0; i < j; i++)
+        printf("%d--%d\t%d\n", result[i].u, result[i].v, result[i].w);
+    printf("Total Weight of the Spanning Tree: %d\n", totalCost);
 }
-int main()
-{
-    int Nodes_8076,Edges_8076;
-    printf("Enter number of nodes and edges \n");
-    scanf("%d %d",&Nodes_8076,&Edges_8076);
-    printf("Enter the following Data %d Edges \n",Edges_8076);
-    printf("<u> <v> <weight> \n");
-    struct Edge arr[Edges_8076];
-    for(int i=0;i<Edges_8076;i++)
-    {
-        scanf("%d %d %d",&(arr[i].u),&(arr[i].v),&(arr[i].weight));
-        printf("\n");
+
+int main() {
+    int i;
+
+    printf("Enter the number of nodes and edges: ");
+    scanf("%d %d", &n, &m);
+
+    printf("Enter the edges (u v w):\n");
+    for (i = 0; i < m; i++) {
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
     }
 
-    sort(arr,Edges_8076);
-    printf("Edges after sorting \n");
-    for(int i=0;i<Edges_8076;i++)
-    {
-        printf("%d %d %d",(arr[i].u),(arr[i].v),(arr[i].weight));
-        printf("\n");
-    }
+    kruskal();
 
-    // for(int i=0;i<Edges_8076;i++)
-    // {
-    //     printf("%d %d %d",(arr[i].u),(arr[i].v),(arr[i].weight));
-    //     printf("\n");
-    // }
-    
-return 0;
+    return 0;
 }
