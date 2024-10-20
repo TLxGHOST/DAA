3e5a497ef81d427500c4df67ce79d4b3908b063b
Scorpion <btejanshu@gmail.com>
2024-10-20 13:46:20 +0530

    easy entering process of multistage graph

--- JAVA/multistage.java
+++ JAVA/multistage.java
@@ -17,18 +17,32 @@ public class multistage
     }
     public void input_matrix(int n)
     {
+        //intializing matrix with 0
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 cost_ajacency[i][j]=0;
             }
         }
+        int k=0;int temp=0;
         for(int i=0;i<n;i++)
-        {   
+        {       //here taking input from user and if user enter -1 you move to next vertex and user wants to connect to a specific vertex enter negative value of the node 
+            // for example if your are entering cost of 1->5 and the next node connection is from 2->6 so you enter -6  and in the next iteration you will be entering values from 2->6
+            // and 2-> further nodes
+            // and if suppose 2 is only conmnected to 6 then enter -1 and this woill move your current pointerr to 3 ->6 and further on 
             for(int j=i+1;j<n;j++)
             {
+                if(k<-1)
+                {
+                 j=Math.abs(k)-1;
+                 temp=j;
+                }
+                else if(k==-1)
+                {
+                    j=temp;
+                }
                 System.out.println("Enter weight ffrom "+(i+1)+" Vertex to "+(j+1)+ " vertex");
-                int k=sc.nextInt();
-                if(k==-1)
+                 k=sc.nextInt();
+                if(k<0)
                 {
                     break;
                 }
@@ -63,7 +77,7 @@ public class multistage
             if(mtrx[i][j]!=0 && mtrx[i][j]+cost[j]<min)
             {
                 min=mtrx[i][j]+cost[j];
-                dest[i]=j;
+                dest[i]=j+1;
             }
         }
         cost[i]=min;
