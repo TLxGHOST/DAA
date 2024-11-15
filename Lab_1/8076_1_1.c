2baf3c1ceb975d7ec3960f9ad3c04cc4d130cadb
Scorpion <btejanshu@gmail.com>
2024-11-15 17:13:21 +0530

    Small changes

--- Lab_1/8076_1_1.c
+++ Lab_1/8076_1_1.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
-void fnd_scnd_sml_lrg_076(int arr_076[], int n, FILE *outputFile) {
+void fnd_scnd_sml_lrg_076(int arr_076[], int n, FILE *outputFile)
+{
     int sml_076 = arr_076[0];
     int scndSml_076 = arr_076[0];
     int lrg_076 = arr_076[0];
     int scndLrgst_076 = arr_076[0];
 
-    for (int i = 1; i < n; i++) {
-        if (arr_076[i] < sml_076) {
+    for (int i = 1; i < n; i++)
+    {
+        if (arr_076[i] < sml_076)
+        {
             scndSml_076 = sml_076;
             sml_076 = arr_076[i];
-        } else if (arr_076[i] < scndSml_076 && arr_076[i] != sml_076) {
+        }
+        else if (arr_076[i] < scndSml_076 && arr_076[i] != sml_076)
+        {
             scndSml_076 = arr_076[i];
         }
 
-        if (arr_076[i] > lrg_076) {
+        if (arr_076[i] > lrg_076)
+        {
             scndLrgst_076 = lrg_076;
             lrg_076 = arr_076[i];
-        } else if (arr_076[i] > scndLrgst_076 && arr_076[i] != lrg_076) {
+        }
+        else if (arr_076[i] > scndLrgst_076 && arr_076[i] != lrg_076)
+        {
             scndLrgst_076 = arr_076[i];
         }
     }
@@ -41,16 +49,17 @@ void sort_find_8076(int arr_076[], int n, FILE *outputFile)
             }
         }
     }
-    
 
     fprintf(outputFile, "Second sml_076 element: %d\n", arr_076[1]);
-    fprintf(outputFile, "Second lrg_076 element: %d\n", arr_076[n-2]);
+    fprintf(outputFile, "Second lrg_076 element: %d\n", arr_076[n - 2]);
 }
-int main() {
+int main()
+{
     FILE *inputFile = fopen("d:/DAA/Lab_1/8076_1_1_in.dat", "r");
     FILE *outputFile = fopen("output_1_1.dat", "w");
 
-    if (inputFile == NULL || outputFile == NULL) {
+    if (inputFile == NULL || outputFile == NULL)
+    {
         printf("Error opening file.\n");
         return 1;
     }
@@ -59,7 +68,8 @@ int main() {
     fscanf(inputFile, "%d", &n);
 
     int arr_076[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         fscanf(inputFile, "%d", &arr_076[i]);
         printf("%d\n", arr_076[i]);
     }
